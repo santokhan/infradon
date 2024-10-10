135ce59a696a7e689de077e0c1c27d8759c785af
LoicFrei <loic.frei@heig-vd.ch>
2024-10-10 11:56:47 +0200

    Addpost+try+initdatabase

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,125 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script lang="ts">
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+import PouchDB from 'Pouchdb'
+declare interface post{
+      _id: string,
+      doc: {
+        post_name: string,
+        post_content: string,
+        attributes: {
+          creation_date: string
+        }
+      }
+    }
+export default {
+
+
+
+  data() {
+
+    return {
+
+      datas: [],
+
+      databaseReference: null,
+
+    };
+
+  },
+
+
+
+  methods: {
+
+
+    inc() {
+
+      // old
+
+      // this.total++;
+
+    },
+//http://admin:admin@localhost:5500/post
+
+
+initDatabase() {
+      const db = new PouchDB('http://admin:admin@localhost:5986/post');
+      if (db) {
+        console.log("Connected to collection 'post'");
+      } else {
+        console.warn("Something went wrong");
+      }
+      
+    }
+
+
+
+    },
+    async addPost(){
+      if (this.db){
+        const newPost:Post = {
+          _id: new Date().toISOString(),
+          doc:{
+            post_name: "Mon premier post",
+            post_content: "ceci est mon premier exemple",
+            attributes: {
+              creation_date: new Date().toISOString(),
+              author:"Loïc Frei"
+            }
+          }
+        },
+        
+        try{
+          const response = await this.db.put(newPost);
+          console.log('Document ajouté avevc succès', response);
+
+        }catch(error){console.error('erreur lors de lajout du document', error)}
+      }
+
+    },
+
+
+
+
+    
+
+
+    fetchData() {
+
+
+    }
+
+
+   
+
+  },
+
+
+
+  mounted() {
+
+    this.initDatabase()
+
   }
+
+
+
 }
-</style>
+
+</script>
+
+
+
+<template>
+
+  <h1>InfraDon2</h1>
+
+  <p>Counter: {{ datas }}</p>
+
+  <button type="button" role="button" @click="inc">
+
+    +1
+
+  </button>
+
+</template>
---
+++ src/views/loicView.vue
@@ -0,0 +1,41 @@
+
+<script lang="ts">
+
+export default {
+  data(){
+    return {
+    total:10,
+  };
+},
+
+
+methods: {
+  inc() {
+    this.total++;
+  },
+  initDatabase(){},
+  fetchDatabase(){}
+}
+}
+
+
+
+</script>
+
+<template>
+    <h1>This is an Loïc page</h1>
+    <p>Counter: {{total}}</p>
+    <button type="button" role="button" @click="inc">+1</button>
+  
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
